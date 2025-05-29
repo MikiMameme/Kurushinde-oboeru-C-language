66a6821f3ea60ad1e63c48cd48cf239d0fffd31a
MikiMameme <milky.is.mama.flavor@gmail.com>
2025-05-29 23:55:49 +0900

    5章練習問題2
    消費税率を10%に変更しています。

---
+++ 5章練習問題2/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int juice, milk, money, payment, change;
+	double tax;
+	
+	juice = 198;
+	milk = 138;
+	money = 1000;
+	tax = 1.10;
+	
+	payment = (int) ((juice + milk * 2) * tax);
+	change = money - payment;
+	
+	printf("%d‰~\n", change);
+	
+	return 0;
+}

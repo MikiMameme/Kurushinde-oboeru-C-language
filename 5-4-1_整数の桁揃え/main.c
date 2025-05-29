9c4a4674cd6bfbafc5a83ff329613d0903c4f685
MikiMameme <milky.is.mama.flavor@gmail.com>
2025-05-29 23:51:53 +0900

    5章4-1 整数の桁揃え

---
+++ 5-4-1_整数の桁揃え/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 10000, b = 500, c = 3;
+	
+	printf("AÇÕ %d Ç≈Ç∑ÅB \n", a);
+	printf("BÇÕ %d Ç≈Ç∑ÅB \n", b);
+	printf("CÇÕ %d Ç≈Ç∑ÅB \n", c);
+	
+	return 0;
+}

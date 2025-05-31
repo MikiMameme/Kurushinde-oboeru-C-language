3e55dd1c4aaff15df796005e3a71ac2b2a6fec5f
MikiMameme <milky.is.mama.flavor@gmail.com>
2025-06-01 01:01:07 +0900

    6章練習問題（定価を入力すると割引の値段を表示）
    定価を入力すると割引の値段を表示するプログラム

---
+++ 6章練習問題2/main.c
@@ -0,0 +1,15 @@
+/* 定価を入力すると割引の値段を表示するプログラム */
+#include <stdio.h>
+
+int main(void)
+{
+	int price;
+	
+	printf("定価を入力してください:");
+	scanf("%d", &price);
+	
+	printf("1割引 = %d円\n", (int) (price * 0.9));
+	printf("3割引 = %d円\n", (int) (price * 0.7));
+	
+	return 0;
+}

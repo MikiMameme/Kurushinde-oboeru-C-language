8dcadf82f808352c0cd2059a5f942a4344bc49a3
MikiMameme <milky.is.mama.flavor@gmail.com>
2025-06-01 23:48:56 +0900

    7章2-3 論理演算子

---
+++ 7-2-3_論理演算子/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int suuti;
+	scanf("%d", &suuti);
+	if (suuti >= 0 && suuti <= 10) printf("‚O`‚P‚O‚ÌŠÔ‚Å‚·B\n");
+	if (!(suuti >= 0 && suuti <=10))printf("‚O`‚P‚O‚ÌŠÔ‚Å‚Í‚ ‚è‚Ü‚¹‚ñB\n");
+	return 0;
+}

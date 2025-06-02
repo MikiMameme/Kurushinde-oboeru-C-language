5f2e6ff65ac3a61f0c5130dd9ecdd638e45cd49d
MikiMameme <milky.is.mama.flavor@gmail.com>
2025-06-02 23:48:42 +0900

    8章2-1 条件が複数の場合の処理

---
+++ 8-2-1_条件が複数の場合の処理/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int age;
+	printf("”N—îF");
+	scanf("%d",&age);
+	if(age <= 3) printf("—c™F–³—¿\n");
+	if(age >= 4 && age <= 12) printf("q‹ŸF‚Q‚T‚O‰~\n");
+	if(age >= 13) printf("‘ålF‚S‚O‚O‰~\n");
+	return 0;
+}

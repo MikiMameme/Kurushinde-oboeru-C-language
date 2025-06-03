8f4cb6a6ca56e5781a0d872ebb107d299e66457d
MikiMameme <milky.is.mama.flavor@gmail.com>
2025-06-03 22:14:12 +0900

    18章練習問題（西暦年を入力するとオリンピックが開かれるか表示）
    西暦年を入力するとオリンピックが開かれるか表示するプログラム。
    （2020年は夏季五輪と表示する）
    オリンピック開催を計算する部分は別の関数として作成。
    関数の戻り値はenum定数で返す。

---
+++ 18章練習問題/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int olympic(int year);
+
+enum {
+	OLYMPIC_NON,
+	OLYMPIC_SUMMER,
+	OLYMPIC_WINTER,
+};
+
+int main(void)
+{
+	int year,hold;
+	
+	scanf("%d",&year);
+	hold = olympic(year);
+	
+	switch(hold) {
+	case OLYMPIC_NON:
+		printf("ŠJ‚©‚ê‚È‚¢\n");
+		break;
+	case OLYMPIC_SUMMER:
+		printf("‰Ä‹GŒÜ—Ö\n");
+		break;
+	case OLYMPIC_WINTER:
+		printf("“~‹GŒÜ—Ö\n");
+		break;
+	};
+	
+	return 0;
+}
+
+int olympic(int year)
+{
+	if(year % 2 == 0){
+		if(year % 4 == 0){
+			return OLYMPIC_SUMMER;
+		}else{
+			return OLYMPIC_WINTER;
+		}
+	}else{
+		return OLYMPIC_NON;
+	}
+	
+}

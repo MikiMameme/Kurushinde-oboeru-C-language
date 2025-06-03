912968e2b08f15938e6e7385c40c551601cea371
MikiMameme <milky.is.mama.flavor@gmail.com>
2025-06-03 21:42:20 +0900

    10章練習問題（点数を入力するプログラム）
    点数を入力するプログラム。
    ０～１００以外の数値を入力された場合、再入力させるようにしてある。

---
+++ 10章練習問題/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	
+	int score = 0;
+	
+	do {
+		if (score != 0) printf("“_”‚Í‚O`‚P‚O‚O‚Ì”ÍˆÍ‚Å“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B\n");
+		printf ("“_”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+		scanf ("%d", &score);
+	} while (score < 0 || score > 100);
+	
+	printf ("“ü—Í‚³‚ê‚½“_” %d\n", score);
+	
+	return 0;
+	
+}
